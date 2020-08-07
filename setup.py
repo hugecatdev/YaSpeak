8b553da197b73a43ba838a59ed1df6e5ebe02423
hugecatdev <53375304+hugecatdev@users.noreply.github.com>
2020-08-07 22:16:05 +0300

    Start

---
+++ setup.py
@@ -0,0 +1,31 @@
+﻿import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yaspeak",
+    version="0.0.1",
+    author="dmitrijkotov",
+    author_email="53375304+hugecatdev@users.noreply.github.com",
+    description="Python wrapper for Yandex text voiceover",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hugecatdev/YaSpeak",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    keywords="speak",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3",
+    install_requires=[
+        "requests"
+    ]
+)
