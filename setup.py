1958fa64b72811b48830b795a745c2846ed46e01
hugecatdev <53375304+hugecatdev@users.noreply.github.com>
2020-08-18 10:20:08 +0300

    -__init__

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yaspeak",
-    version="0.0.1",
+    version="0.0.2",
     author="dmitrijkotov",
     author_email="53375304+hugecatdev@users.noreply.github.com",
     description="Python wrapper for Yandex text voiceover",
